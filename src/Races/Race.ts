8b813de2bb848437d655486d54abaa1507868cc0
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-09-25 17:39:16 -0300

    criação da classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,26 @@
+export default abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string { 
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
