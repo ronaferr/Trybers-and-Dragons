368d0c9e0e9486228ef9afa0d36640f3a609cc49
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-09-27 17:31:52 -0300

    criação de interface SimpleFighter

---
+++ src/Character.ts
@@ -0,0 +1,18 @@
+/* import Energy from './Energy';
+import Fighter from './Fighter';
+import Race from './Races';
+
+export default class Character implements Fighter {
+  private race: Race;
+  private Archetype = this.Archetype;
+  private maxLifePoints: number;
+  private lifePoints: number;
+  private strength: number;
+  private defense: number;
+  private dexterity: number;
+  private energy: Energy;
+
+  constructor(name: string) {
+
+  }
+} */
